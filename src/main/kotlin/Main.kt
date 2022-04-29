833d9515346b70b2bc051552d6855972facfb19e
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-04-29 07:14:18 +0700

    Composing suspend function lazily

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,5 @@
 import kotlinx.coroutines.*
 import kotlin.system.measureTimeMillis
-import kotlin.time.measureTime
 
 fun main(args: Array<String>) = runBlocking{// creates a blocking corotine that executes in current thread (Main Thread)
     // Try adding program arguments via Run/Debug configuration.
@@ -11,9 +10,17 @@ fun main(args: Array<String>) = runBlocking{// creates a blocking corotine that 
 
     val time = measureTimeMillis {
         // Function getMessageOne() and getMessageTwo() will be executed concurrently. Means getMessageTwo() doesn't need to wait for getMessageOne() is executed
-        val messageOne: Deferred<String> = async { getMessageOne() }; // "async" in this line make the execution of getMessageOne() happens in the background thread
-        val messageTwo: Deferred<String> = async { getMessageTwo() } // "async" in this line make the execution of getMessageTwo() happens in the background thread
-        println("The entire message is: ${messageOne.await()} ${messageTwo.await()}"); // this statement (println) will wait for executing of getMessageOne() and getMessageTwo() are finished because we added "await()" in each message
+        /*
+        * - "async" in this line make the execution of getMessageOne() happens in the background thread
+        * - "start = CoroutineStart.LAZY" make this line doesn't get executed when "messageOne" never be called but make the coroutine is suspended
+        * */
+        val messageOne: Deferred<String> = async(start = CoroutineStart.LAZY) { getMessageOne() };
+        /*
+        * - "async" in this line make the execution of getMessageTwo() happens in the background thread
+        * - "start = CoroutineStart.LAZY" make this line doesn't get executed when "messageTwo" never be called but make the coroutine is suspended
+        * */
+        val messageTwo: Deferred<String> = async(start = CoroutineStart.LAZY) { getMessageTwo() };
+        println("The entire message is: ${messageOne.await() + " "+ messageTwo.await()}"); // this statement (println) will wait for executing of getMessageOne() and getMessageTwo() are finished because we added "await()" in each message
     }
 
     println("Completed in $time ms");
@@ -23,10 +30,12 @@ fun main(args: Array<String>) = runBlocking{// creates a blocking corotine that 
 
 suspend fun getMessageOne(): String {
     delay(1000); // pretends to do some work
+    println("After working in getMessageOne()");
     return "Hello";
 }
 
 suspend fun getMessageTwo(): String {
     delay(1000); // pretends to do some work
+    println("After working in getMessageTwo()");
     return "World";
 }
