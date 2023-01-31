3247d8cea1071a724ea161da8cf6906413631534
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-01-31 11:20:29 +0700

    Floating Point Number

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,9 @@
 fun main(args: Array<String>) {
-    // -128 to 127
-    var age: Byte = 30
-    println("Byte: $age")
-    // -32768 to 32767
-    var distance: Short = 2000
-    println("Short: $distance")
-    // -2147483648(-2^31) to -2147483647((-2^31) - 1)
-    var height: Int = 120
-    println("Int: $height")
-    // -9223372036854775808(-2^63) to -9223372036854775807((-2^63) - 1)
-    // add "L" to make a number becomes Long
-    var balance: Long = 100000000L
-    println("Long: $balance")
+    // 6 to 7 digits after decimal point
+    // Add F to make a floating value becomes float
+    var value: Float = 23.2F
+    println("Float: $value")
+    // 15 to 16 digits after decimal point
+    var radius: Double = 2345.54535
+    println("Double: $radius")
 }
