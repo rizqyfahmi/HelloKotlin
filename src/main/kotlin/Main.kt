287f78f0b8f23af6a79efa07f75d12de64eb1112
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-01-31 14:36:39 +0700

    Convertion Number

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,15 @@
 fun main(args: Array<String>) {
-    // -128 to 127
-    var age: Byte = 3_0
-    println("Byte: $age")
-    // -32768 to 32767
-    var distance: Short = 2_000
-    println("Short: $distance")
-    // -2147483648(-2^31) to -2147483647((-2^31) - 1)
-    var height: Int = 1_2_0
-    println("Int: $height")
-    // -9223372036854775808(-2^63) to -9223372036854775807((-2^63) - 1)
-    // add "L" to make a number becomes Long
-    var balance: Long = 100_000_000L
-    println("Long: $balance")
+    var number: Int = 100
+    var byte: Byte = number.toByte()
+    println("toByte: $byte")
+    var short: Short = number.toShort()
+    println("toShort: $short")
+    var int: Int = number.toInt()
+    println("toInt: $int")
+    var long: Long = number.toLong()
+    println("toLong: $long")
+    var float: Float = number.toFloat()
+    println("toFloat: $float")
+    var double: Double = number.toDouble()
+    println("toDouble: $double")
 }
